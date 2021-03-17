1f351f778a35a17f3f7f3b57e2697df41fae7676
Mariano Gonzalez <56511727+marianoGnlz@users.noreply.github.com>
2021-03-17 08:45:00 -0300

    asd

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,64 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo(){
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+        .then(localMediaStream => {
+            video.srcObject  = localMediaStream;
+            video.play();
+        })
+        .catch(err => console.error('Oh No!!!!'));
+
+}
+
+function paintToCanvas(){
+    const { videoWidth: width, videoHeight: height } = video;
+    canvas.width = width;
+    canvas.height = height;
+    setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+        let pixels = ctx.getImageData(0 , 0, width, height);
+
+        // pixels = redEffect(pixels);
+        pixels = rgbSplit(pixels);
+        
+        // ctx.globalAlpha = 0.1;
+
+        ctx.putImageData(pixels, 0, 0);
+    }, 16);
+}
+
+function takePhoto(){
+    snap.currentTime = 0 ;
+    snap.play();
+
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.innerHTML = `<img src="${data}" alt="handsome man">`;
+    strip.insertBefore(link, strip.firstChild);
+
+}
+function redEffect(pixels){
+    for (let i = 0; i < pixels.data.length; i+=4) {
+        pixels.data[i + 0] = pixels.data[i + 0] + 200; 
+        pixels.data[i + 1] = pixels.data[i + 1] - 50;
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5;
+    }
+    return pixels;
+}
+function rgbSplit(pixels){
+    for (let i = 0; i < pixels.data.length; i+=4) {
+        pixels.data[i - 150] = pixels.data[i + 0]; 
+        pixels.data[i + 100] = pixels.data[i + 1];
+        pixels.data[i - 150] = pixels.data[i + 2];
+    }
+    return pixels;
+}
+
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
